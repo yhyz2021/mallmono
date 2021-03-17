fbec8deacc846e9a32a27bfb928642f77759a902
1zca0 <yizheng1991@hotmail.com>
2021-03-17 17:21:34 +0800

    Initial

---
+++ src/main/java/pro/mm/mallmajor/MallmajorApplication.java
@@ -0,0 +1,13 @@
+package pro.mm.mallmajor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallmajorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallmajorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/mm/mallmajor/MallmajorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.mm.mallmajor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallmajorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
